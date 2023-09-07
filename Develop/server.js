b75fe93daa1c5bbc28d3bc3b7550ce0ee06bb9b5
Devin Amlen <devinamlen@gmail.com>
2023-09-07 10:53:57 -0400

    Create server.js
    initial commit and what have you

---
+++ Develop/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+const db = require('./db/db.json');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//reportRequest: a debug function meant to report any and all requests to the console.
+const reportRequest = function(req){
+  console.info(`${req.method} request recieved for endpoint ${req.originalUrl}.`);
+}
+
+// GET request for Notes page
+app.get('/api/notes', (req,res) => {
+  reportRequest(req);
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  res.status(200).json();
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
