65530bae807d187644be2913cfe7e4ad9e7ea690
Devin Amlen <devinamlen@gmail.com>
2023-09-08 14:01:09 -0400

    Update index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -27,13 +27,14 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
-const getNotes = () =>
+const getNotes = () => {
+  console.log("getting notes");
   fetch('/api/notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
-  });
+  })};
 
 const saveNote = (note) =>
   fetch('/api/notes', {
