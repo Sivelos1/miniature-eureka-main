a86d9cf22c0ee6551e0cdaa210fe702da0999c7d
Devin Amlen <devinamlen@gmail.com>
2023-09-09 12:25:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -65,10 +65,7 @@ app.delete('/api/notes/:id', (req,res) => {
   if(req.params.id){
     var note = db.find(n => n.id === req.params.id);
       if(note){
-        console.log('before: '+JSON.stringify(db));
-        delete note;
         removeItemOnce(db, note);
-        console.log('after:'+JSON.stringify(db));
         fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(db));
         res.status(200).json({message:`Successfully deleted element #${req.params.id}`});
         console.info(`Status code 200 - note successfully deleted!`);
